d209debc3d366e754ea23195bea9b23d833b5d7d
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-13 21:15:23 +0200

    Add active-link wrappers around navbar entries and an app-canvas component
    The app-canvas component holds the whole body of the app and automatically
    invokes foundation() on every element.

---
+++ app/routes/index.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model: function() {
+		return Ember.RSVP.hash({
+			backend: Ember.$.getJSON('https://api.github.com/repos/philip98/obsidian-web/commits?per_page=5')
+				.then(this.prepareData),
+			frontend: Ember.$.getJSON('https://api.github.com/repos/philip98/obsidian-web-frontend/commits?per_page=5')
+				.then(this.prepareData)
+		});
+	},
+	prepareData: (data) => {
+		data.forEach((r) => {
+			r.commit.author.date = (new Date(r.commit.author.date)).toLocaleString();
+			r.sha = r.sha.substring(0, 6);
+		});
+		return data;
+	}
+});
