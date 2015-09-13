ec966eae09d6b17a39fb126dfffc3b8cc6752fde
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-13 21:13:29 +0200

    Add ember-simple-auth, ember-cli-active-link-wrapper, and foundation's js

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,15 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'default-src': "'self'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' https://api.github.com",
+      'img-src': "'self'",
+      'media-src': "'self'",
+      'style-src': "'self' 'unsafe-inline'"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
