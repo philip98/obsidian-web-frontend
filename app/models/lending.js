ec07da86cc3a4123b4724f2c35e24c50407543ae
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-27 14:17:40 +0200

    Rename lending's lender attribute/relationship to person (like it is in the API)

--- app/models/lending.js
+++ app/models/lending.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	createdAt: DS.attr(),
 	book: DS.belongsTo('book', {async: true}),
-	lender: DS.belongsTo('person', {polymorphic: true})
+	person: DS.belongsTo('person', {polymorphic: true})
 });
