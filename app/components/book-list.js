2a70667ec27d03d85adadccf1ae499d6fe410e75
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-30 17:32:30 +0200

    Pull common functionality of *-list into common superclass

--- app/components/book-list.js
+++ app/components/book-list.js
@@ -1,5 +1,3 @@
-import Ember from 'ember';
+import AbstractList from 'obsidian-web/components/abstract-list';
 
-export default Ember.Component.extend({
-	tagName: 'table'
-});
+export default AbstractList.extend();
