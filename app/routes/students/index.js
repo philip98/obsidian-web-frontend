5d494052db35770ce144f6c6100183f17a90d911
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-30 17:30:02 +0200

    Pull isAuthenticated into get() method @route.model()

--- app/routes/students/index.js
+++ app/routes/students/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.findAll('student').catch(() => {
-			if (!this.controllerFor('students.index').get('auth').isAuthenticated) {
+			if (!this.controllerFor('students.index').get('auth.isAuthenticated')) {
 				this.get('flashMessages').warning('Sie müssen sich zuerst einloggen');
 				return this.transitionTo('login');
 			}
--- app/routes/students/return-base.js
+++ app/routes/students/return-base.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model(params) {
 		return this.store.findRecord('student', params.id).catch(() => {
-			if (!this.controllerFor('students.return-base').get('auth').isAuthenticated) {
+			if (!this.controllerFor('students.return-base').get('auth.isAuthenticated')) {
 				this.get('flashMessages').warning('Sie müssen sich zuerst einloggen');
 				return this.transitionTo('login');
 			}
