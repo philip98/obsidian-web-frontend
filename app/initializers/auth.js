61ba71c310749df239e44b005d78cd1930fa3aa7
Philip Schlösser <philipschloesser98@gmail.com>
2015-10-08 16:32:35 +0200

    Make addAlias modal open on button click (by changing the button's tag to a)
    Furthermore, startup now initiates a call to GET `data_host`/schools instead of
    OPTIONS `data_host`.

--- app/initializers/auth.js
+++ app/initializers/auth.js
@@ -2,10 +2,7 @@ import ENV from 'obsidian-web/config/environment';
 
 export function initialize(container, application) {
 	application.inject('controller', 'auth', 'service:auth');
-	$.ajax({
-		url: ENV.data_host,
-		method: 'options'
-	});
+	$.get(ENV.data_host + '/schools').fail();
 }
 
 export default {
