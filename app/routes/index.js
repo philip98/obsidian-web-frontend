b493c1c47987461e1d7ead79e4b7576259925262
Philip Schlösser <philipschloesser98@gmail.com>
2015-10-11 10:53:27 +0200

    Fix link to help.md on github

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return Ember.$.ajax({
-            url: 'https://api.github.com/repos/philip98/obsidian-web-frontend/help.md',
+            url: 'https://api.github.com/repos/philip98/obsidian-web-frontend/contents/help.md',
             dataType: 'text',
             accepts: {
                 text: 'application/vnd.github.3.html'
