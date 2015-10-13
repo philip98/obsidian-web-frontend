0df7fef92db77a3a31ae801d6bcf07591e5a671b
Philip Schlösser <philipschloesser98@gmail.com>
2015-10-13 09:02:28 +0200

    Remove empty init() from isbn-entry.js

--- app/components/isbn-entry.js
+++ app/components/isbn-entry.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	init() {
-		this._super(...arguments);
-	},
 	tagName: 'li',
 	autofocus: Ember.computed.equal('index', 0),
 	actions: {
