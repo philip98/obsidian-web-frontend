1757e656aaa6ad8b477c6b717a61d1c10e051730
Philip Schlösser <philipschloesser98@gmail.com>
2015-10-30 20:07:37 +0100

    Add import-dialog component.
    This component holds a text-file-upload and a few inputs for
    determining header, common class, ...

---
+++ app/components/import-dialog.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    init() {
+        this.klass = null;
+        this.dataReveal = true;
+        this.files = null;
+        this.header = 'name';
+        this._super(...arguments);
+    },
+    classNames: ['reveal-modal', 'small'],
+    attributeBindings: ['id', 'dataReveal:data-reveal'],
+    filesSelected: Ember.computed.and('files', 'header'),
+    actions: {
+        submit() {
+
+        }
+    }
+})
