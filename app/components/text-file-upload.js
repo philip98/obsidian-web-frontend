dcda486cad783fbfa48b21f96f3996e2548065f7
Philip Schlösser <philipschloesser98@gmail.com>
2015-11-01 19:46:31 +0100

    Remove double type: 'file';

--- app/components/text-file-upload.js
+++ app/components/text-file-upload.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
     tagName: 'input',
     type: 'file',
     attributeBindings: ['multiple', 'type'],
-    type: 'file',
     change(event) {
         this.sendAction('action', event.target.files);
     }
