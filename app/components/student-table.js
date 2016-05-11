4a42d6af14166ac2fb1e08e4e554cc1363634837
Philip Schlösser <philipschloesser98@gmail.com>
2016-05-11 13:14:08 +0200

    Extend Promise madness to studentTable
    The student table now also uses Promises (since the baseSets come in the form
    of Promises and incomplete data would otherwise be displayed)

--- app/components/student-table.js
+++ app/components/student-table.js
@@ -1,16 +1,29 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
 	tagName: 'table',
 	classNames: ['table-header-rotated'],
-	studentsExtended: Ember.computed('students', 'students.@each.baseSets', 'books', function() {
-		return this.get('students').map((student) => {
-			return {
-				student: student,
-				baseSets: this.get('books').map((book) => {
-					return student.get('baseSets').filterBy('book.id', book.get('id')).length === 1;
-				})
-			};
+	studentsExtended: Ember.computed('students', 'students.@each.baseSets.@each.book.id', 'books', function() {
+		let promise = Ember.RSVP.all(this.get('students').map((student) => {
+			return student.get('baseSets').then((baseSets) => {
+				return Ember.RSVP.all(baseSets.map((bs) => {
+					return bs.get('book');
+				}));
+			}).then((bsBooks) => {
+				let ids = bsBooks.map((bb) => {
+					return Number(bb.get('id'));
+				});
+				return {
+					student: student,
+					baseSets: this.get('books').map((book) => {
+						return ids.indexOf(Number(book.get('id'))) > -1;
+					})
+				};
+			});
+		}));
+		return DS.PromiseObject.create({
+			promise: promise
 		});
 	})
 });
