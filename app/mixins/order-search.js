df3bb4fabef36b1c9d48edda4f84157a986f4a3d
Philip Schlösser <philipschloesser98@gmail.com>
2016-06-24 16:18:38 +0200

    Restrict length of every OrderSearchable controller's output to 100

--- app/mixins/order-search.js
+++ app/mixins/order-search.js
@@ -14,9 +14,9 @@ export default Ember.Mixin.create({
     sortedData: Ember.computed('searchedData', 'sorting', function() {
         if (this.get('sorting')[0] === '-') {
             return this.get('searchedData').sortBy(this.get('sorting').slice(1))
-                .reverseObjects();
+                .reverseObjects().slice(0, 99);
         } else {
-            return this.get('searchedData').sortBy(this.get('sorting'));
+            return this.get('searchedData').sortBy(this.get('sorting')).slice(0, 99);
         }
     }),
     actions: {
