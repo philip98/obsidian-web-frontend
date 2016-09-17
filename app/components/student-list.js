3b4e3dc402fe424a944c2e22537bc69cf590d8b8
Philip Schlösser <philipschloesser98@gmail.com>
2016-09-17 10:21:13 +0200

    Add "Select all" button to student-list

--- app/components/student-list.js
+++ app/components/student-list.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: 'table',
+	data: {},
+	checkAll: false,
+	checkAllObserver: Ember.observer('checkAll', function() {
+		this.get('data').forEach((st) => {
+			st.set('checked', this.get('checkAll'));
+		});
+	}),
 	actions: {
 		sortName() {
 			this.sendAction('action', 'name');
